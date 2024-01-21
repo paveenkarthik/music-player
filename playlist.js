afd90dc6331640e14fb56106f7eb9e217405b632
PAVEEN KARTHIK S <paveensenthil15@gmail.com>
2024-01-21 20:43:17 +0530

    second commit

---
+++ playlist.js
@@ -0,0 +1,10 @@
+function openplay(){
+    var h=document.getElementById("mid");
+    h.style.height="100%"
+    h.style.width=" calc(100% - 375px)"
+    h.src="sign.html";
+    var g=document.getElementById("bb");
+    g.style.cursor="allowed";
+    var f=document.getElementsByClassName("front-back-button");
+    f.style.cursor="allowed";
+}
